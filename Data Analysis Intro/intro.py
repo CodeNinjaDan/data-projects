adfeb0476fa9aa9246e5af4d4d5f63a9892ea037
CodeNinjaDan <dnlmuiru@gmail.com>
2024-10-30 22:34:24 +0300

    Added Dict Comprehension

--- Data Analysis Intro/intro.py
+++ Data Analysis Intro/intro.py
@@ -34,6 +34,21 @@ data_dict = {
     "scores": [70, 56, 89]
 }
 
-data2 = pandas.DataFrame(data_dict)
-print(data2)
+# data2 = pandas.DataFrame(data_dict)
+# print(data2)
 #data2.to_csv() ->Create a new csv file.
+
+#Use .iterrow() for dictionary comprehension in a dataframe
+#{key_expression: value_expression for item in iterable if condition}
+
+student_data_frame = pandas.DataFrame(data_dict)
+print(student_data_frame)
+
+#Loop through a data frame
+#for (key, value) in student_data_frame.items():
+    #print(value)
+
+#Loop through rows of DataFrame
+for (index, row) in student_data_frame.iterrows():
+    if row.students == "Amy":
+        print(row.student)
